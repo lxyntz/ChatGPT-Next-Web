27c9c4815a96e44138e257a2744f8f23d26bed3f
LIMITED <lxyntz@gmail.com>
2023-05-26 13:07:44 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>GPT chatbot</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          AI is imminent and AI is for you.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
